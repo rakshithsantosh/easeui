eaefaedfecfe6e65ff2954971d4cd365c5cb441b
rakshith_s <rakshithsantosh@gmail.com>
2025-05-20 14:03:37 +0530

    fumadocs is installed and the configurations are set

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { createMDX } from "fumadocs-mdx/next";
 
+const withMDX = createMDX();
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
