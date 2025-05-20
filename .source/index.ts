43665c0c91e42fbb340aa7924067079241b102a5
rakshith_s <rakshithsantosh@gmail.com>
2025-05-20 17:53:09 +0530

    customizing and setting up configs for fumadocs

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1747729118873"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1747740662752"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/Rakshith/OneDrive/Desktop/Projects/easeui/content/docs/index.mdx"}, data: docs_0 }], [])
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/Rakshith/OneDrive/Desktop/Projects/easeui/content/docs/index.mdx"}, data: docs_0 }]);
+export const meta = _runtime.meta<typeof _source.meta>([]);
---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "EaseUi",
+  url: "#",
+  ogImage: "#",
+  description:
+    "Copy,Paste,Customize a collection of modren, interactive, customizable ui components, quick and easy to use",
+  links: {
+    github: "#",
+    twitter: "#",
+  },
+};
