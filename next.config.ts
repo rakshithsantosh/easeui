566c25a9c6c1bc2257d40448dfd4a5c12ceb44e9
rakshith_s <rakshithsantosh@gmail.com>
2025-05-26 10:23:46 +0530

    creating registry

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,35 @@
-import type { NextConfig } from "next";
 import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
-const nextConfig: NextConfig = {
+
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  outputFileTracingIncludes: {
+    "/**": ["components/codesnippetui/**/*"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/r/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         hostname: "*",
       },
     ],
+  },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
